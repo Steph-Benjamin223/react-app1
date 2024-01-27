136c1b22be51d3b1833909b5250fa34f42660ab1
Steph-Benjamin223 <stephanie.benjamin@gmail.com>
2024-01-27 17:41:06 -0600

    final

--- src/components/news-feed.js
+++ src/components/news-feed.js
@@ -5,12 +5,33 @@ let e = React.createElement;
 
 export default class NewsFeed extends React.Component {
   render() {
-    return e(
-      "div",
-      { class: "container" },
-      e(Post, {}, null),
-      e(Post, {}, null),
-      e(Post, {}, null)
+    let comments = [
+      {
+        content: "This is my post",
+        username: "Tommy",
+        date: "12-12-2018",
+      },
+      {
+        content: "This is another post",
+        username: "Sammy",
+        date: "12-17-2018",
+      },
+      {
+        content: "More posts from us",
+        username: "Sally",
+        date: "12-19-2018",
+      },
+    ];
+
+    return (
+      <div className="container">
+        <Post
+          {...{ comments: comments, content: "This is my post content!" }}
+        />
+
+        <Post {...{ content: "Here is another post!!!" }} />
+        <Post />
+      </div>
     );
   }
 }
--- src/components/post.js
+++ src/components/post.js
@@ -6,25 +6,48 @@ import Comment from "./comment";
 let e = React.createElement;
 
 export default class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: props.comments,
+      content: props.content,
+    };
+  }
   render() {
-    return e(
-      "div",
-      { class: "card w-75" },
-      e(
-        "div",
-        { class: "card-header bg-success text-white" },
-        "Username and Time"
-      ),
-      e("div", { class: "card-body" }, "Post Content"),
-      e(
-        "div",
-        { class: "card-footer" },
-        e(LikeButton, {}, null),
-        e("span", {}, " "),
-        e(ReplyButton, {}, null),
-        e("br", {}, null),
-        e(Comment, {}, null)
-      )
+    //let commentOne = {
+    // content: "This is my post",
+    // username: "Tommy",
+    //  date: "12-12-2018",
+    // };
+    // let commentTwo = {
+    //  content: "This is another post",
+    //  username: "Sammy",
+    // date: "12-17-2018",
+    // };
+    // let commentThree = {
+    //  content: "More posts from us",
+    // username: "Sally",
+    //  date: "12-19-2018",
+    //  };
+    let comments = [];
+    if (this.state.comments) {
+      for (let comment of this.state.comments) {
+        comments.push(<Comment {...comment} />);
+      }
+    }
+    return (
+      <div className="card w-75">
+        <div className="card-header bg-success text-white">
+          Username and Time
+        </div>
+        <div className="card-body">{this.state.content}</div>
+
+        <div className="card-footer">
+          <LikeButton />
+          <ReplyButton />
+          {comments}
+        </div>
+      </div>
     );
   }
 }
